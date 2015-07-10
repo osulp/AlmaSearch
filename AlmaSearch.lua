97f7dbe417a3212e8f7d32cc9fc0e4f180b5e2d1
Gregorio Luis Ramirez <luisram90_101@hotmail.com>
2015-07-10 09:20:16 -0700

    updated add on to display the browser

--- AlmaSearch.lua
+++ AlmaSearch.lua
@@ -4,17 +4,30 @@ requestCountForm.Form = nil;
 requestCountForm.Grid = nil;
 requestCountForm.TextEdit = nil;
 requestCountForm.RibbonPage = nil;
+--settings.AddonRibbonName = GetSetting("AddonRibbonName");
+
 connection = nil;
 
 function Init()
   connection = CreateManagedDatabaseConnection();
   connection:Connect();
   interfaceMngr = GetInterfaceManager();
-  interfaceMngr:ShowMessage("Got hurrrrrrr", "Yo");
-  requestCountForm.Form = interfaceMngr:CreateForm("Search", "Script");
-  requestCountForm.RibbonPage = requestCountForm.Form:CreateRibbonPage("AlmaSearch");
+  --interfaceMngr:ShowMessage("Got hurrrrrrr", "Yo");
+  requestCountForm.Form = interfaceMngr:CreateForm("AlmaSearch", "Script");
+  
+  requestCountForm.Browser = requestCountForm.Form:CreateBrowser("AlmaSearch", "AlmaSearch", "AlmaSearch");
+  
+  -- Hide the text label                                                                                                                                                                                                      
+  requestCountForm.Browser.TextVisible = false;
+
+  --Suppress Javascript errors                                                                                                                                                                                                
+  requestCountForm.Browser.WebBrowser.ScriptErrorsSuppressed = true; 
+  
+  requestCountForm.RibbonPage = requestCountForm.Form:GetRibbonPage("AlmaSearch");
   requestCountForm.RibbonPage:CreateButton("AlmaSearch", GetClientImage("Search32"), "AlmaSearch", "AlmaSearch");
   requestCountForm.Form:Show();
+  requestCountForm.Browser:Navigate("http://osulibrary.oregonstate.edu");
+  
   connection:Dispose();
 end
 
